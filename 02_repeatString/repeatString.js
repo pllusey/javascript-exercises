0b7e008f707651785ec7690b70f8d484e05fbc64
pllusey <108682741+pllusey@users.noreply.github.com>
2024-08-20 09:58:45 +0100

    completed excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let count = 0;
+  let repeatedString = "";
 
+  if (!string) {
+    return repeatedString;
+  } else if (num < 0) {
+    return "ERROR";
+  }
+
+  while (count < num) {
+    for (let i = 0; i < string.length; i++) {
+      repeatedString += string[i];
+    }
+    count++;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
