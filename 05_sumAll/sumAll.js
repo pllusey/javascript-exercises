48bf6b0a407f9599886cb7dcc3ab8d8db5d9ede1
pllusey <108682741+pllusey@users.noreply.github.com>
2024-08-20 16:52:27 +0100

    05 first attempt

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(minVal, maxVal) {
+    const difference = maxVal - minVal;
+    let sum = "";
 
+    for (let i=0; i<difference-1; i++) {
+        return sum += i;
+    }
 };
 
 // Do not edit below this line
