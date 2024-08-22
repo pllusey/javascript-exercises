7b4d5fcb4322f114234fc7c652017d79341ff4d2
pllusey <108682741+pllusey@users.noreply.github.com>
2024-08-22 07:21:27 +0100

    06 first attempt

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 && year % 400) {
+        return 
+    } else if (year % 4) {
+        return
+    } return
 };
 
 // Do not edit below this line
