cacf1b1499c0632bf10f1fba8742fe3f7d4d4882
pllusey <108682741+pllusey@users.noreply.github.com>
2024-08-22 08:01:21 +0100

    07 attempt 3 pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,23 @@
-const convertToCelsius = function(fahrenheit) {
-  const celsius = Math.floor((fahrenheit - 32) * (5/9))
-  return !Number.isInteger(celsius) ? celsius.toFixed(1) : celsius
+const convertToCelsius = function (fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  if (celsius == 0) {
+    return celsius;
+  } else {
+    return Number.parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function(celsius) {
-  const fahrenheit = Math.floor((celsius * (9/5)) + 32);
-  return !Number.isInteger(fahrenheit) ? fahrenheit.toFixed(1) : fahrenheit
+const convertToFahrenheit = function (celsius) {
+  const fahrenheit = celsius * (9 / 5) + 32;
+  if (fahrenheit == 32) {
+    return fahrenheit;
+  } else {
+    return Number.parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
